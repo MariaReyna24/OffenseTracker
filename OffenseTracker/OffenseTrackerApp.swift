3f43e047a04656ecc9ed62866f048a885b618036
Maria Reyna <maria.reyna2244@gmail.com>
2024-02-12 17:44:01 -0500

    Initial Commit

---
+++ OffenseTracker/OffenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffenseTrackerApp.swift
+//  OffenseTracker
+//
+//  Created by Maria Reyna on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OffenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
