8c92bd717aa5ef30df55d4dde750562925843c37
Maria Reyna <maria.reyna2244@gmail.com>
2024-07-02 13:58:21 -0400

    Initial commit

---
+++ OffenseTracker/Views/CommentSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentSectionView.swift
+//  OffenseTracker
+//
+//  Created by Maria Reyna on 7/1/24.
+//
+
+import SwiftUI
+
+struct CommentSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentSectionView()
+}
